7d8bf07310e69f236efefba403c4e6a95240c2e0
Carla <carla.f.bates@hotmail.com>
2019-02-06 22:26:15 -0500

    assignment-2

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -11,4 +11,17 @@ describe("routes : static", () => {
             });
         });
     });
+
+    describe("GET /marco", () => {
+        it("should return 'polo'", (done) => {
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
+    });
+
+
+
+
 });
