202ed9078bc047fac3b6e92282eef23563b5e1c0
Carla <carla.f.bates@hotmail.com>
2019-02-07 21:01:34 -0500

    assignment-2 revised

--- src/app.js
+++ src/app.js
@@ -4,4 +4,8 @@ const app = express();
 const routeConfig = require("./config/route-config.js");
 routeConfig.init(app);
 
+app.use("/marco", (req, res, next) => {
+    res.send("polo")
+});
+
 module.exports = app;
