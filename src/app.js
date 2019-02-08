3cd2e799adab109c24a198a4cb3561ea1a256cd9
Carla <carla.f.bates@hotmail.com>
2019-02-08 15:23:46 -0500

    WIP delpoy to production

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 
+const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
+appConfig.init();
 routeConfig.init(app);
 
 app.use("/marco", (req, res, next) => {
-    res.send("polo")
+	res.send("polo");
 });
 
 module.exports = app;
