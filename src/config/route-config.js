3e756091bb8139fa7c79d231d561222d7abca851
Carla <carla.f.bates@hotmail.com>
2019-02-13 22:27:26 -0500

    checkpoint-5 complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,8 @@
 module.exports = {
     init(app) {
         const staticRoutes = require("../routes/static");
+        const topicsRoutes = require("../routes/topics");
         app.use(staticRoutes);
+        app.use(topicsRoutes);
     }
 }
---
+++ src/routes/topics.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const topicsController = require("../controllers/topicsController");
+
+router.get("/topics", topicsController.index);
+
+module.exports = router;
