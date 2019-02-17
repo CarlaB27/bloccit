9706bee4e1b3d3b6655ab156aef6d9d7626d28f3
Carla <carla.f.bates@hotmail.com>
2019-02-16 20:39:54 -0500

    checkpoint 6 complete

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -4,5 +4,11 @@ const router = express.Router();
 const topicsController = require("../controllers/topicsController");
 
 router.get("/topics", topicsController.index);
+router.get("/topics/new", topicsController.new);
+router.post("/topics/create", topicsController.create);
+router.get("/topics/:id", topicsController.show);
+router.post("/topics/:id/destroy", topicsController.destroy);
+router.get("/topics/:id/edit", topicsController.edit);
+router.post("/topics/:id/update", topicsController.update);
 
 module.exports = router;
