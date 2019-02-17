74889e2ec55b31c082875eabb28187a2d97100fc
Carla <carla.f.bates@hotmail.com>
2019-02-16 22:48:51 -0500

    assignment 6

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,7 +2,9 @@ module.exports = {
     init(app) {
         const staticRoutes = require("../routes/static");
         const topicsRoutes = require("../routes/topics");
+        const advertisementsRoutes = require("../routes/advertisements");
         app.use(staticRoutes);
         app.use(topicsRoutes);
+        app.use(advertisementsRoutes);
     }
 }
