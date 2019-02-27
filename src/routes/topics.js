2f7ae2cd4c077777543f2f840238b8d1835a3ada
Carla <carla.f.bates@hotmail.com>
2019-02-26 23:07:17 -0500

    assignment-10 complete

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
 const topicsController = require("../controllers/topicsController");
+const validation = require("./validation");
 
 router.get("/topics", topicsController.index);
 router.get("/topics/new", topicsController.new);
-router.post("/topics/create", topicsController.create);
+router.post("/topics/create", validation.validateTopics, topicsController.create);
 router.get("/topics/:id", topicsController.show);
 router.post("/topics/:id/destroy", topicsController.destroy);
 router.get("/topics/:id/edit", topicsController.edit);
-router.post("/topics/:id/update", topicsController.update);
+router.post("/topics/:id/update", validation.validateTopics, topicsController.update);
 
 module.exports = router;
