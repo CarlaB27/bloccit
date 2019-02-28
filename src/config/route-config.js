13b3a5add487b439b1edddea91a8fc8d71de1d13
Carla <carla.f.bates@hotmail.com>
2019-02-27 22:00:15 -0500

    checkpoint-11 complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,8 +3,10 @@ module.exports = {
         const staticRoutes = require("../routes/static");
         const topicsRoutes = require("../routes/topics");
         const postRoutes = require("../routes/posts");
+        const userRoutes = require("../routes/users");
         app.use(staticRoutes);
         app.use(topicsRoutes);
         app.use(postRoutes);
+        app.use(userRoutes);
     }
 }
